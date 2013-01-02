62fbe7020f8e93861163d277a619dd687e727df8
Emelyanov Aleksey <pndr@ya.ru>
2013-01-02 17:17:56 +0400

    Метод find_check вынес из области видимости.

--- find.js
+++ find.js
@@ -1,3 +1,4 @@
+;(function(){
 
     //https://developer.mozilla.org/en/JavaScript/Reference/Global_Objects/Array/filter#Compatibility/////////////////////////////
     //
@@ -33,6 +34,7 @@
     //
     //https://developer.mozilla.org/en/JavaScript/Reference/Global_Objects/Array/filter#Compatibility/////////////////////////////
 
+
     //https://developer.mozilla.org/en/JavaScript/Reference/Global_Objects/Array/some/////////////////////////////////////////////
     //
         if (!Array.prototype.some)
@@ -61,13 +63,6 @@
         }
     //
     //https://developer.mozilla.org/en/JavaScript/Reference/Global_Objects/Array/some/////////////////////////////////////////////
-
-
-    //это массив?
-    /*function is_array( mixed_var )
-      {
-    return ( mixed_var instanceof Array );
-    }*/
 
 
     //добавляем для массива метод indexOf - содержит ли массив значение needle, если да, то вернёт его номер в массиве, если нет, то вернёт -1
@@ -109,135 +104,138 @@
     //добавляем для массива метод indexOf - содержит ли массив значение needle, если да, то вернёт его номер в массиве, если нет, то вернёт -1
 
 
+
+    //			 function(условия, проверяемый_объект)
+  	find_check = function(terms  , checking_obj      )
+  	{
+  			/*
+  				terms == {
+  								name: "%rua",
+  								price: ">100"
+  				}
+
+  				checking_obj == {
+  									name:"Chianti Classico",
+  									price:"540.00",
+  									year:"Array"
+  				}
+  			*/
+
+  			var checked = true;
+  			for( key in terms )//перебираем все параметры в условие
+  			{
+  					/*
+  						checking_obj == {
+  											name:"Chianti Classico",
+  											price:"540.00",
+  											year:"Array"
+  						}
+
+  						checking_obj_value == "Chianti Classico";
+  					*/
+
+  					if(!checking_obj){ checked = false; continue; } //если проверяемый объект не объект
+  					if(!checking_obj[key]){ checked = false; continue; } //если проверяемый объект не содержит ключа, который есть в условии
+
+  					var term_value = terms[key]; // ">1000"
+  					var checking_obj_value = checking_obj[key];// "4000"
+
+  					//console.log('условие: ', term_value);
+
+  					if(( Object.prototype.toString.call( term_value ) === '[object Array]' ))//( is_array( term_value ) )//пресеты фильтрующие ИЛИ
+  					{
+  							/*
+  								term_value = [ >111, 222 ]
+  							*/
+  							checked = term_value.some(function( term_value_inner ){
+  								//console.log('term_value_inner:', term_value_inner, '  checking_obj_value:', checking_obj_value);
+  								//console.log('[].find_check({ x:term_value_inner },{ x:checking_obj_value}):', [].find_check({ x:term_value_inner },{ x:checking_obj_value}));
+  								//return (checking_obj_value == term_value_inner);
+  								return find_check({ x:term_value_inner },{ x:checking_obj_value});
+  							});
+  					}
+  					else
+  					{
+  							var parsing_array = /^(%?|<=?|<?|>?|>=?|><?)([^%<=>]+)(%?)$/i.exec( term_value );
+
+  							//console.log(parsing_array);
+
+  							var prolog_term = parsing_array[1];
+  							var body_term = parsing_array[2];
+  							var epilog_term = parsing_array[3]
+
+  							//console.log('prolog_term:', prolog_term, '  body_term:', body_term, '  epilog_term:' ,epilog_term);
+
+  							if( prolog_term == '>=' )//пресеты фильтрующие
+  							{
+  									//console.log(  );
+  									checked = checked && ( parseFloat(checking_obj_value) >= parseFloat(body_term)) ;
+  							}
+  							else if( prolog_term == '<=' )//пресеты фильтрующие
+  							{
+  									//console.log(  );
+  									checked = checked &&
+  									(
+  										parseFloat(checking_obj_value) <= parseFloat(body_term)
+  									);
+  							}
+  							else if( prolog_term == '><' )//пресеты фильтрующие
+  							{
+  									//console.log(  );
+  									var parsing_body_term_array = /^([^;]+)(;+)([^;]+)$/i.exec( body_term );
+  									var body_term_first = parsing_body_term_array[1];
+  									var body_term_second = parsing_body_term_array[3];
+
+  									//console.log('body_term_first=',body_term_first,' body_term_second=',body_term_second);
+
+  									checked = checked &&
+  									(
+  										parseFloat(checking_obj_value) >= parseFloat(body_term_first)
+  										&&
+  										parseFloat(checking_obj_value) <= parseFloat(body_term_second)
+  									);
+  							}
+  							else if( prolog_term == '>' )//пресеты фильтрующие
+  							{
+  									//console.log(  );
+  									checked = checked && ( parseFloat(checking_obj_value) > parseFloat(body_term) );
+  							}
+  							else if( prolog_term == '<' )//пресеты фильтрующие
+  							{
+  									//console.log(  );
+  									checked = checked && ( parseFloat(checking_obj_value) < parseFloat(body_term) );
+  							}
+  							else if( prolog_term == '%' && epilog_term == '%' )//пресеты фильтрующие по вхождению подстроки
+  							{
+  									checked = checked && ( checking_obj_value.toLowerCase().indexOf( body_term.toLowerCase() ) >= 0 );
+  							}
+  							else if( prolog_term == '%' )//пресеты фильтрующие по началу подстроки ( "%text" )
+  							{
+  									checked = checked && ( checking_obj_value.toLowerCase().indexOf( body_term.toLowerCase() ) == (checking_obj_value.length - body_term.length ) );
+  							}
+  							else if( epilog_term == '%' )//пресеты фильтрующие по концу подстроки ( "text%" )
+  							{
+  									checked = checked && ( checking_obj_value.toLowerCase().indexOf( body_term.toLowerCase() ) == 0 );
+  							}
+  							else//иначе простое сравнение на равенство
+  							{
+  									//console.log('checking_obj_value: '+checking_obj_value + '  term_value:' + term_value + '  checking_obj.NAME:' + checking_obj.NAME);
+  									checked = checked && ( checking_obj_value == term_value );
+  							}
+  					}
+
+
+  					if(!checked) return false;//если какое-то условие не прошло, то дальше продолжать не имеет смысла.
+  			}
+
+  			return checked;
+  	}
+
+
+
     //добавляем для массива метод find
     if (!Array.prototype.find)
     {
-    	//						     function(условия, проверяемый_объект)
-    	Array.prototype.find_check = function(terms  , checking_obj      )
-    	{
-    			/*
-    				terms == {
-    								name: "%rua",
-    								price: ">100"
-    				}
-
-    				checking_obj == {
-    									name:"Chianti Classico",
-    									price:"540.00",
-    									year:"Array"
-    				}
-    			*/
-
-    			var checked = true;
-    			for( key in terms )//перебираем все параметры в условие
-    			{
-    					/*
-    						checking_obj == {
-    											name:"Chianti Classico",
-    											price:"540.00",
-    											year:"Array"
-    						}
-
-    						checking_obj_value == "Chianti Classico";
-    					*/
-
-    					if(!checking_obj){ checked = false; continue; } //если проверяемый объект не объект
-    					if(!checking_obj[key]){ checked = false; continue; } //если проверяемый объект не содержит ключа, который есть в условии
-
-    					var term_value = terms[key]; // ">1000"
-    					var checking_obj_value = checking_obj[key];// "4000"
-
-    					//console.log('условие: ', term_value);
-
-    					if(( term_value instanceof Array ))//( is_array( term_value ) )//пресеты фильтрующие ИЛИ
-    					{
-    							/*
-    								term_value = [ >111, 222 ]
-    							*/
-    							checked = term_value.some(function( term_value_inner ){
-    								//console.log('term_value_inner:', term_value_inner, '  checking_obj_value:', checking_obj_value);
-    								//console.log('[].find_check({ x:term_value_inner },{ x:checking_obj_value}):', [].find_check({ x:term_value_inner },{ x:checking_obj_value}));
-    								//return (checking_obj_value == term_value_inner);
-    								return [].find_check({ x:term_value_inner },{ x:checking_obj_value});
-    							});
-    					}
-    					else
-    					{
-    							var parsing_array = /^(%?|<=?|<?|>?|>=?|><?)([^%<=>]+)(%?)$/i.exec( term_value );
-
-    							//console.log(parsing_array);
-
-    							var prolog_term = parsing_array[1];
-    							var body_term = parsing_array[2];
-    							var epilog_term = parsing_array[3]
-
-    							//console.log('prolog_term:', prolog_term, '  body_term:', body_term, '  epilog_term:' ,epilog_term);
-
-    							if( prolog_term == '>=' )//пресеты фильтрующие
-    							{
-    									//console.log(  );
-    									checked = checked && ( parseFloat(checking_obj_value) >= parseFloat(body_term)) ;
-    							}
-    							else if( prolog_term == '<=' )//пресеты фильтрующие
-    							{
-    									//console.log(  );
-    									checked = checked &&
-    									(
-    										parseFloat(checking_obj_value) <= parseFloat(body_term)
-    									);
-    							}
-    							else if( prolog_term == '><' )//пресеты фильтрующие
-    							{
-    									//console.log(  );
-    									var parsing_body_term_array = /^([^;]+)(;+)([^;]+)$/i.exec( body_term );
-    									var body_term_first = parsing_body_term_array[1];
-    									var body_term_second = parsing_body_term_array[3];
-
-    									//console.log('body_term_first=',body_term_first,' body_term_second=',body_term_second);
-
-    									checked = checked &&
-    									(
-    										parseFloat(checking_obj_value) >= parseFloat(body_term_first)
-    										&&
-    										parseFloat(checking_obj_value) <= parseFloat(body_term_second)
-    									);
-    							}
-    							else if( prolog_term == '>' )//пресеты фильтрующие
-    							{
-    									//console.log(  );
-    									checked = checked && ( parseFloat(checking_obj_value) > parseFloat(body_term) );
-    							}
-    							else if( prolog_term == '<' )//пресеты фильтрующие
-    							{
-    									//console.log(  );
-    									checked = checked && ( parseFloat(checking_obj_value) < parseFloat(body_term) );
-    							}
-    							else if( prolog_term == '%' && epilog_term == '%' )//пресеты фильтрующие по вхождению подстроки
-    							{
-    									checked = checked && ( checking_obj_value.toLowerCase().indexOf( body_term.toLowerCase() ) >= 0 );
-    							}
-    							else if( prolog_term == '%' )//пресеты фильтрующие по началу подстроки ( "%text" )
-    							{
-    									checked = checked && ( checking_obj_value.toLowerCase().indexOf( body_term.toLowerCase() ) == (checking_obj_value.length - body_term.length ) );
-    							}
-    							else if( epilog_term == '%' )//пресеты фильтрующие по концу подстроки ( "text%" )
-    							{
-    									checked = checked && ( checking_obj_value.toLowerCase().indexOf( body_term.toLowerCase() ) == 0 );
-    							}
-    							else//иначе простое сравнение на равенство
-    							{
-    									//console.log('checking_obj_value: '+checking_obj_value + '  term_value:' + term_value + '  checking_obj.NAME:' + checking_obj.NAME);
-    									checked = checked && ( checking_obj_value == term_value );
-    							}
-    					}
-
-
-    					if(!checked) return false;//если какое-то условие не прошло, то дальше продолжать не имеет смысла.
-    			}
-
-    			return checked;
-    	}
-
     	Array.prototype.find = function(condition /*, from*/)
     	{
 
@@ -245,11 +243,11 @@
 
     			result = this.filter(function( element ){
 
-    				return ( _self.find_check( condition, element ) );
+    				return ( find_check( condition, element ) );
 
     			});
 
-    			//разные варианты одного и того же, выбирал вариант по шустрее
+    			//1//разные варианты одного и того же, выбирал вариант по шустрее
     			/*result = function(_self){
     										return _self.filter(function(element){
 
@@ -258,7 +256,7 @@
     										});
     			}(this);*/
 
-
+                //2//разные варианты одного и того же, выбирал вариант по шустрее
     			/*result = this.filter(function(_self){
     													return function( element ){
 
@@ -267,7 +265,7 @@
     													}
     			}(this));*/
 
-
+                //3//разные варианты одного и того же, выбирал вариант по шустрее
     			/*for (; from < len; from++)//перебираем все элементы массива
     			{
     				if( this.find_check(condition, this[from]) )//для каждого элемента массива проверяем условие, если оно выполняется добавлем элемент в result
@@ -282,7 +280,7 @@
     				return [];
     	};
 
-              Array.prototype.pages = function(elt)
+        Array.prototype.pages = function(elt)
     	{
     			//catalog.pages({ page:0, elements_on_page:12 });
     			var len = this.length >>> 0, from = (elt.page)*elt.elements_on_page, to = (elt.page+1)*elt.elements_on_page, result = [];  if(to>len)to=len;
@@ -290,17 +288,18 @@
     			result.count_of_pages = Math.ceil( len / elt.elements_on_page );
     			result.current_page = elt.page;
 
-                      //console.log('from('+from+') < to('+to+')');
+                //console.log('from('+from+') < to('+to+')');
     			for (; from < to; from++)
     			{
     					result.push(this[from]);
     			}
 
-                      //console.log('pages.result',result,'pages.length='+result.length);
 
+                //console.log('pages.result',result,'pages.length='+result.length);
     			if( result.length > 0 )
     				return result;
     			else
     				return [];
     	};
     }
+})();
